2a2f8a680523a80f5fd518d4831e18dc07af68a9
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-20 11:07:31 +0200

    initial commit and displayStudents exercise completed

---
+++ destructuring/displayStudents.js
@@ -0,0 +1,19 @@
+/*
+Write a function called displayStudentInfo which accepts an object and returns the string "Your full name is" concatenated with the value of the first key and a space and then the value of the last key. See if you can destructure this object inside of the function.
+
+Examples:
+    displayStudentInfo({first: 'Elie', last:'Schoppik'}) // 'Your full name is Elie Schoppik')
+*/
+
+let student = {
+  first: "Elie",
+  last: "Shopik",
+};
+
+function displayStudentInfo(student) {
+  let { first, last } = student;
+  return `Your full name is ${first} ${last}.`;
+}
+
+let div = document.querySelector(".div");
+div.textContent = displayStudentInfo(student);
